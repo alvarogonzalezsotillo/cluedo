529710f2c75a939fca0148fbccda9f54d4f6485c
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2017-07-31 15:52:36 +0200

    Ya funciona and or not, ahora a por un número de trues

--- js/cp.js
+++ js/cp.js
@@ -17,7 +17,7 @@ function describe(o){
 }
 
 function log(s){
-    console.log(s);
+    //console.log(s);
 }
 
 function assert(b){
@@ -28,23 +28,63 @@ function assert(b){
 
 
 
-function CP(name){
-    this._name=name;
-    this._dependants = [];
+function CP(name, observed ){
+
+    this._name =name;
+    this._containers = [];
+    this._observed = [];
+    if( observed ){
+        this._observed = observed;
+    }
+
+    for( var  i = 0 ; i < this._observed.length ; i++ ){
+        this._observed[i].addContainer(this);
+    }
 }
 
 
 
 CP.prototype = {
 
-    addDependant : function(d){
-        this._dependants.push(d);
+    addContainer : function(d){
+        this._containers.push(d);
     },
 
-    notifyDependants : function(){
-        for( var i = 0 ; i < this._dependants.length ; i++ ){
-            this._dependants[i].reduceOwnDomain();
+    notifyContainers : function(){
+        log( this.name() + ": notifyContainers ");
+        for( var i = 0 ; i < this._containers.length ; i++ ){
+            var cp = this._containers[i];
+            log( "  " + this.name() + ": notifyContainers: " + cp.name() );
+            cp.notified();
         }
+    },
+
+    notified: function(){
+        this.reduceOwnDomain();
+        this.reduceObservedDomain();
+    },
+
+    describe : function(level){
+
+        var log = console.log;
+        if( !level ){
+            level = 0;
+            log( "DESCRIBE: " + this.name() );
+        }
+        
+        var s = "";
+        for( var i = 0 ; i < level ; i++ ){
+            s += "  ";
+        }
+        log( s + this.toString() );
+
+        for( var i = 0; i < this.observed().length ; i++ ){
+            this.observed()[i].describe(level+1);
+        }
+    },
+
+    observed : function(){
+        return this._observed;
     },
     
     toString: function(){
@@ -52,7 +92,7 @@ CP.prototype = {
     },
 
     defined: function(){
-        return this.canBeTrue() ^ this.canBeFalse();
+        return (this.canBeTrue() && !this.canBeFalse()) || (!this.canBeTrue() && this.canBeFalse());
     },
     
     name: function(){
@@ -80,16 +120,18 @@ CP.prototype = {
     },
 
     reduceOwnDomain: function(){
+        return false;
     },
 
-    reduceOthersDomain: function(){
+    reduceObservedDomain: function(){
+        return false;
     }
 
 
 };
 
-function CPBoolean(name){
-    CP.call(this,name);
+function CPBoolean(name,observed){
+    CP.call(this,name,observed);
     this._canBeTrue = true;
     this._canBeFalse = true;
 }
@@ -97,17 +139,20 @@ function CPBoolean(name){
 
 InheritAndExtend(CP,CPBoolean, {
     remove: function(value){
+        log( this.name() + ": remove:" + value );
         if( value && this._canBeTrue ){
+            log( "  " + this.name() + ": remove: removed true");
             this._canBeTrue = false;
-            this.reduceOthersDomain();
-            this.notifyDependants();
+            this.reduceObservedDomain();
+            this.notifyContainers();
             return true;
         }
         
         if( !value && this._canBeFalse ){
+            log( "  " + this.name() + ":  remove: removed false");
             this._canBeFalse = false;
-            this.reduceOthersDomain();
-            this.notifyDependants();
+            this.reduceObservedDomain();
+            this.notifyContainers();
             return true;
         }
         
@@ -115,11 +160,91 @@ InheritAndExtend(CP,CPBoolean, {
     },
 });
 
+function CPNumberTrue(cps,number){
+    this._number = number;
+    var names = "";
+    for( var i = 0 ; i < cps.length ; i++ ){
+        names += " " + cps[i].name();
+    }
+    CP.call(this,"AreTrue(" + number + ")(" + names + ")", cps );
+}
+
+InheritAndExtend(CPBoolean,CPNumberTrue, {
+    
+    reduceOwnDomain: function(){
+        var cps = this.observed();
+
+        var log = function(){};
+        
+        var falseNumber = 0;
+        var trueNumber = 0;
+        for( var i = 0 ; i < cps.length ; i++ ){
+            if( cps[i].isFalse() ){
+                falseNumber += 1;
+            }
+            if( cps[i].isTrue() ){
+                trueNumbe += 1;
+            }
+        }
+
+        var ret = false;
+        
+        if( anyFalse ){
+            log( this.name() + ": anyFalse:" + anyFalse + ": since some are false, I can not be true");
+            ret |= this.remove(true);
+        }
+        if( allTrue ){
+            log( this.name() + ": allTrue:" + allTrue + ": since all are true, I can not be false" );
+            ret |= this.remove(false);
+        }
+        return ret;
+    },
+
+    reduceObservedDomain: function(){
+        var cps = this.observed();
+        var log = function(){};
+        
+        if( this.isTrue() ){
+            log( this.name() + ": since I am true, all my observed can not be false");
+            var ret = false;
+            for( var i = 0 ; i < cps.length ; i++ ){
+                ret |= cps[i].remove(false);
+            }
+            return ret;
+        }
+
+        if( this.isFalse() ){
+            log( this.name() + ": since I am false, at least one of my observed can not be true");
+
+            // if only one cp can be false, it should be false
+            var undefinedCP = undefined;
+            var numberOfTrue = 0;
+            for( var i = 0 ; i < cps.length ; i++ ){
+                var cp = cps[i];
+                log( "  " + cp.toString() );
+                if( cp.isTrue() ){
+                    log( "  " + cp.toString() + " is true");
+                    numberOfTrue += 1;
+                }
+                if( !cp.defined() ){
+                    log( "  " + cp.toString() + " is not defined");
+                    undefinedCP = cp;
+                }
+            }
+            var ret = false;
+            if( numberOfTrue == cps.length - 1 && undefinedCP ){
+                log( "  " + this.name() + ": only " + undefinedCP.name() + "(defined:" + undefinedCP.defined() + ") can be false, so removed true from it" );
+                ret = undefinedCP.remove(true);
+            }
+            return ret;
+        }
+    }
+});
+
 
 function CPNot(cp){
-    CP.call(this,"Not(" + cp.name() + ")" ); 
+    CP.call(this,"Not(" + cp.name() + ")", [cp] ); 
     this._cp = cp;
-    this._cp.addDependant(this);
 }
 
 InheritAndExtend(CP,CPNot, {
@@ -139,14 +264,25 @@ InheritAndExtend(CP,CPNot, {
     remove: function(value){
         var changed = this._cp.remove(!value);
         if( changed ){
-            this.notifyDependants();
+            this.notifyContainers();
         }
         return changed;
     },
 
     reduceOwnDomain: function(){
-        this._cp.reduceOwnDomain();  
-    }
+        var ret = this._cp.reduceOwnDomain();
+        if( ret ){
+            this.notifyContainers();
+        }
+    },
+
+    notified: function(){
+        this.notifyContainers();
+    },
+
+    reduceObservedDomain: function(){
+        return this._cp.reduceObservedDomain();
+    },
 
 });
 
@@ -157,146 +293,99 @@ function CPAnd(cps){
     for( var i = 0 ; i < cps.length ; i++ ){
         names += " " + cps[i].name();
     }
-    CPBoolean.call(this,"And(" + names + ")" );
-    this._cps = cps;
-    this._canBeTrue = true;
-    this._canBeFalse = true;
-    for( var i = 0 ; i < this._cps.length ; i++ ){
-        this._cps[i].addDependant(this);
-    }
+    CPBoolean.apply(this, ["And(" + names + ")", cps] );
 }
 
 
 InheritAndExtend(CPBoolean,CPAnd, {
     
     reduceOwnDomain: function(){
-        for( var i = 0 ; i < this._cps.length ; i++ ){
-            this._cps[i].reduceOwnDomain();
-        }
+        var cps = this.observed();
 
+        var log = function(){};
+        
         var anyFalse = false;
         var allTrue = true;
-        for( var i = 0 ; i < this._cps.length ; i++ ){
-            if( this._cps[i].isFalse() ){
+        for( var i = 0 ; i < cps.length ; i++ ){
+            if( cps[i].isFalse() ){
                 anyFalse = true;
                 allTrue = false;
             }
-            if( !this._cps[i].defined() ){
+            if( !cps[i].defined() ){
                 allTrue = false;
             }
         }
+
+        var ret = false;
+        
         if( anyFalse ){
-            this.remove(true);
+            log( this.name() + ": anyFalse:" + anyFalse + ": since some are false, I can not be true");
+            ret |= this.remove(true);
         }
         if( allTrue ){
-            this.remove(false);
+            log( this.name() + ": allTrue:" + allTrue + ": since all are true, I can not be false" );
+            ret |= this.remove(false);
         }
+        return ret;
     },
 
-    reduceOthersDomain: function(){
+    reduceObservedDomain: function(){
+        var cps = this.observed();
+        var log = function(){};
+        
         if( this.isTrue() ){
-            for( var i = 0 ; i < this._cps.length ; i++ ){
-                this._cps[i].remove(false);
+            log( this.name() + ": since I am true, all my observed can not be false");
+            var ret = false;
+            for( var i = 0 ; i < cps.length ; i++ ){
+                ret |= cps[i].remove(false);
             }
+            return ret;
         }
 
         if( this.isFalse() ){
+            log( this.name() + ": since I am false, at least one of my observed can not be true");
+
             // if only one cp can be false, it should be false
             var undefinedCP = undefined;
             var numberOfTrue = 0;
-            for( var i = 0 ; i < this._cps.length ; i++ ){
-                var cp = this._cps[i];
+            for( var i = 0 ; i < cps.length ; i++ ){
+                var cp = cps[i];
+                log( "  " + cp.toString() );
                 if( cp.isTrue() ){
+                    log( "  " + cp.toString() + " is true");
                     numberOfTrue += 1;
                 }
                 if( !cp.defined() ){
+                    log( "  " + cp.toString() + " is not defined");
                     undefinedCP = cp;
                 }
             }
-            if( numberOfTrue == this._cps.length - 1 ){
-                cp.remove(true);
+            var ret = false;
+            if( numberOfTrue == cps.length - 1 && undefinedCP ){
+                log( "  " + this.name() + ": only " + undefinedCP.name() + "(defined:" + undefinedCP.defined() + ") can be false, so removed true from it" );
+                ret = undefinedCP.remove(true);
             }
+            return ret;
         }
     }
 });
-
-
-function CPOr(cps){
-    assert(cps.length > 0);
-    var names = "";
-    for( var i = 0 ; i < cps.length ; i++ ){
-        names += " " + cps[i].name();
-    }
-    CPBoolean.call(this,"Or(" + names + ")" );
-    this._cps = cps;
-    this._canBeTrue = true;
-    this._canBeFalse = true;
-    for( var i = 0 ; i < this._cps.length ; i++ ){
-        this._cps[i].addDependant(this);
-    }
-}
-
-InheritAndExtend(CPBoolean,CPOr, {
-    
-    reduceOwnDomain: function(){
-
-        for( var i = 0 ; i < this._cps.length ; i++ ){
-            this._cps[i].reduceOwnDomain();
-        }
-        
-        var anyTrue = false;
-        var allFalse = true;
-        for( var i = 0 ; i < this._cps.length ; i++ ){
-            if( this._cps[i].isTrue() ){
-                anyTrue = true;
-                allFalse = false;
-            }
-            if( !this._cps[i].defined() ){
-                allFalse = false;
-            }
-        }
-        if( anyTrue ){
-            this.remove(false);
-        }
-        if( allFalse ){
-            this.remove(true);
-        }
-    },
-
-    reduceOthersDomain: function(){
-        if( this.isFalse() ){
-            for( var i = 0 ; i < this._cps.length ; i++ ){
-                this._cps[i].remove(true);
-            }
-        }
-
-        if( this.isTrue() ){
-            // if only one cp can be true, it should be true
-            var undefinedCP = undefined;
-            var numberOfFalse = 0;
-            for( var i = 0 ; i < this._cps.length ; i++ ){
-                var cp = this._cps[i];
-                if( cp.isFalse() ){
-                    numberOfFalse += 1;
-                }
-                if( !cp.defined() ){
-                    undefinedCP = cp;
-                }
-            }
-            if( numberOfFalse == this._cps.length - 1 ){
-                cp.remove(false);
-            }
-        }
-    }
-
-});
-
 
 
 CP.Boolean = function(name){ return new CPBoolean(name); };
 CP.And = function(cps){ return new CPAnd(cps); };
-CP.Or = function(cps){ return new CPOr(cps); };
 CP.Not = function(cp){ return new CPNot(cp); };
+CP.Or = function(cps){
+    var negatedCPS = [];
+    var names = "";
+    for( var i = 0 ; i < cps.length ; i++ ){
+        negatedCPS.push( CP.Not(cps[i]) );
+        names += cps[i].name() + " ";
+    }
+    var ret =  CP.Not( CP.And(negatedCPS) );
+    ret.name = function(){ return "Or(" + names + ")"; };
+    return ret;
+};
+
 
 
 function test(){
@@ -329,8 +418,11 @@ function test(){
 
             a.remove(true);
 
+            andAB.describe();
+
             assert(andAB.isFalse());
         },
+
 
         function(){
             var a = CP.Boolean("a");
@@ -343,12 +435,92 @@ function test(){
             assert(b.isTrue());
         },
 
+
+        function(){
+            var a = CP.Boolean("a");
+            var b = CP.Boolean("b");
+
+            var orAB = CP.Or([a,b]);
+
+            a.remove(false);
+
+            assert(orAB.isTrue());
+        },
+
+
+        function(){
+            var a = CP.Boolean("a");
+            var b = CP.Boolean("b");
+
+            var orAB = CP.Or([a,b]);
+
+            orAB.remove(true);
+
+            assert(a.isFalse());
+            assert(b.isFalse());
+        },
+
+        function(){
+            var a = CP.Boolean("a");
+            var b = CP.Boolean("b");
+
+            var orAB = CP.Or([a,b]);
+
+            orAB.remove(false);
+            b.remove(true);
+
+            orAB.describe();
+
+            assert(a.isTrue());
+        },
+
+        function(){
+            var a = CP.Boolean("a");
+            var b = CP.Boolean("b");
+            var c = CP.Boolean("c");
+            var and = CP.And([a,b,c]);
+            and.remove(false);
+            assert(a.isTrue());
+            assert(b.isTrue());
+            assert(c.isTrue());
+        },
+
+        function(){
+            var a = CP.Boolean("a");
+            var b = CP.Boolean("b");
+            var c = CP.Boolean("c");
+            var or = CP.Or([a,b,c]);
+            or.remove(true);
+            assert(a.isFalse());
+            assert(b.isFalse());
+            assert(c.isFalse());
+        },
+
+        function(){
+            var a = CP.Boolean("a");
+            var b = CP.Boolean("b");
+            var c = CP.Boolean("c");
+            var or = CP.Or([a,b,c]);
+            or.remove(false);
+            a.remove(true);
+            b.remove(true);
+
+            or.describe();
+            
+            assert(a.isFalse());
+            assert(b.isFalse());
+            assert(c.isTrue());
+        }
+        
     ];
 
+    
     for( var i = 0 ; i < tests.length ; i++ ){
         log( "----- Test " + i );
         tests[i]();
     }
+    
+    
 }
 
 test();
